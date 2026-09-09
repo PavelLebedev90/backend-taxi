f21e7e5a23653410ad1c21811ea86e3e63aa8c49
Pavel <pashoktver90@gmail.com>
2026-09-09 14:17:11 +0300

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   testEnvironment: "node",
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
   testMatch: ["**/__tests__/**/*.spec.ts", "**/__tests__/**/*.test.ts"],
 };
+
+export default config;
